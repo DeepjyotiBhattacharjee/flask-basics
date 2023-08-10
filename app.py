a34e7ad9e4fb4a2d0ae0b55a11ff6703f283df22
DeepjyotiBhattacharjee <deepjyotibhattacharjee217@gmail.com>
2023-08-10 15:09:08 +0530

    final commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 ## Create a simple flask application
 
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect,url_for
 
 # create the flask app
 app = Flask(__name__)
@@ -25,12 +25,26 @@ def success(score):
 def fail(score):
     return "The person has failed and the score is "+str(score)
 
-@app.route('/calculate')
+@app.route('/calculate',methods=['POST','GET'])
 def calculate():
-    return render_template("calculate.html")
+    if request.method == 'GET':
+        return render_template("calculate.html")
+    else :
+        maths = float(request.form['maths'])
+        science = float(request.form['science'])
+        history = float(request.form['history'])
 
-@app.route('/calculatemarks')
-def calculate_marks():...
+        average_marks = (maths+science+history)/3
+        result = ""
+
+        if average_marks>=50:
+            result = "success"
+        else:
+            result = "fail"
+        # return redirect(url_for(result,score = average_marks))
+
+        return render_template('result.html',results = average_marks)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
